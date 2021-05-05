2288cebdba6e85c10ea1f8d40b9b29d5929b87bd
Alexandre Soares Paiva <xpaiva@hotmail.com>
2021-05-05 20:13:29 -0300

    Aula 231 OrdersScreen

---
+++ lib/views/orders_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Orders'),
+      ),
+      body: Center(
+        child: Text('Order items'),
+      ),
+    );
+  }
+}
