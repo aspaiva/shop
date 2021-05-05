b49e3824dc1eff135fa25d64b9d837978312fc62
Alexandre Soares Paiva <xpaiva@hotmail.com>
2021-05-04 21:41:37 -0300

    Multiprovider, ainda sem uso do Cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_detail_screen.dart';
@@ -10,8 +11,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(), //create: a new provider, not a reuse
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ], //create: a new provider, not a reuse
       child: MaterialApp(
         title: 'Minha Loja',
         theme: ThemeData(
